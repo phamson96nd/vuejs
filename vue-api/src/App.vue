e8a658f48dc570d8cf5731f7a90391c8fc075c41
sonpv@ominext <sonpv@ominext.com>
2021-03-18 00:27:14 +0700

    1

--- vue-api/src/App.vue
+++ vue-api/src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <div>
+            <button @click="auth('facebook')">facebook</button>
+            <button @click="auth('google')">google</button>
+            <button @click="auth1('twitter')">twitter</button>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        methods: {
+            async auth(network) {
+                const hello = this.hello;
+                hello(network).login().then(async () => {
+                    const authRes = hello(network).getAuthResponse();
+                    console.log('11ps', authRes)
+                    await this.axios.post("http://127.0.0.1:8000/api/checkuser", {token: authRes.access_token}).then((response) => {
+                        console.log(response.data)
+                    })
+
+                    /*
+                      performs operations using the token from authRes
+                    */
+                    hello(network).api('me').then(function (json) {
+                        const profile = json;
+                        console.log('333ps', profile)
+                        /*
+                          performs operations using the user info from profile
+                        */
+                    });
+                })
+            },
+            auth1(network) {
+                const hello = this.hello;
+                hello.init({
+                    twitter: '944OPRu0Y6d4dQFfc8FklCVKF'
+                }, {
+                    scope: 'email',
+                    redirect_uri: 'https://192.168.1.141:8080/dashboard'
+                });
+                hello(network).login().then(() => {
+                    const authRes = hello(network).getAuthResponse();
+                    console.log('11ps', authRes)
+                    /*
+                      performs operations using the token from authRes
+                    */
+                    hello(network).api('me').then(function (json) {
+                        const profile = json;
+                        console.log('333ps', profile)
+                        /*
+                          performs operations using the user info from profile
+                        */
+                    });
+                })
+            }
+        }
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- vue-api/src/main.js
+++ vue-api/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloJs from 'hellojs/dist/hello.all.min'
+import VueHello from 'vue-hellojs'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+HelloJs.init({
+  google: '995568444460-b7astq8crnts5nco40heqcjobg7vg4kn.apps.googleusercontent.com',
+  facebook: '765661614347054'
+}, {
+  redirect_uri: 'https://phamson.com:8080/dashboard'
+});
+Vue.use(VueHello, HelloJs);
+Vue.use(VueAxios, axios)
+
 
 new Vue({
   render: h => h(App),
